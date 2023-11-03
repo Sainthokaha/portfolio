6d1105111a51eaec600d5dae3f6235b16dd70851
sainthokaha5@gmail.com <88292276+Sainthokaha@users.noreply.github.com>
2023-11-03 14:50:31 +0100

    third commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,7 +5,8 @@ import { MotionPlugin } from '@vueuse/motion'
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import { ZiggyVue} from '../../vendor/tightenco/ziggy/dist/vue.m';
+import Ziggy from 'ziggy';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
